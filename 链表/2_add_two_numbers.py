6e83b168a24abd105ea51bdfb82934c280290341
zhouang <316447676@qq.com>
2018-07-30 15:11:47 +0800

    1.no_2 2数相加 暴力方法

--- 链表/2_add_two_numbers.py
+++ 链表/2_add_two_numbers.py
@@ -5,12 +5,12 @@ class ListNode(object):
 
 
 class Solution(object):
-    def addTwoNumbers(self, l1: ListNode, l2: ListNode):
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) :
         """
         :type l1: ListNode
         :type l2: ListNode
         :rtype: ListNode
-        思入: 默认l1为长链,若l2比较长,则将l2多出的链转移到l1上去. 然后大数加法的通用代码来完成
+        思入: 默认l1为长链,若l2比较长,则将l2多出的链转移到l1上去. 然后大数加法的通用代码来完成. 空间复杂度O(1)
         """
         carry = 0
         l1_node, l2_node = l1, l2
@@ -30,6 +30,34 @@ class Solution(object):
                 break
             l1_node, l2_node = l1_node.next, l2_node.next if l2_node else None
         return l1
+
+
+class Solution1:
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        思如: 暴力法,创建第3个链,作为结果链表. 不断添加新节点 空间复杂度O(m+n)
+        """
+        l1_node, l2_node = l1, l2
+        result = ListNode(0)
+        my_result = result
+        carry = 0
+        while l1_node or l2_node:
+            l = l1_node.val if l1_node else 0
+            r = l2_node.val if l2_node else 0
+
+            my_sum = l + r + carry
+            carry = my_sum // 10
+            my_result.next = ListNode(my_sum % 10)
+            my_result = my_result.next
+
+            l1_node, l2_node = l1_node.next if l1_node else None, l2_node.next if l2_node else None
+
+        if carry != 0:
+            my_result.next = ListNode(carry)
+        return result.next
 
 
 if __name__ == '__main__':
