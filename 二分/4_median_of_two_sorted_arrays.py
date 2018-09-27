0778f832761e95e67fb3238ca9d335c346c477c8
zhouang <316447676@qq.com>
2018-09-27 18:41:48 +0800

    1.no_4 在2个排序数组中寻找中位数 (原理+越界解释)

--- 二分/4_median_of_two_sorted_arrays.py
+++ 二分/4_median_of_two_sorted_arrays.py
@@ -9,29 +9,31 @@ class Solution:
         一旦出现其他情况,就要大小来调整切的位置(二分)
         复杂度: 只在最小数组上进行二分--->O(log(min(m+n)))
         """
-        # 确定最小数组
-        left, right, nums_small, nums_big = (0, len(nums1), nums1, nums2) if len(nums1) <= len(nums2) else (
-            0, len(nums2), nums2, nums1)
-        size = len(nums1) + len(nums2)
-        while left <= right:
-            cut1 = (left + right) // 2
-            cut2 = size // 2 - cut1  # 算出第2刀位置
-
-            # 为什么切在2端时,要选择赋值最小 or 最大值? 并不影响. 因为最后有一个min和max操作. 极值是不会被取到的
-            L1 = -99999999 if cut1 == 0 else nums_small[cut1 - 1]
-            R1 = 99999999 if cut1 == len(nums_small) else nums_small[cut1]
-            L2 = -99999999 if cut2 == 0 else nums_big[cut2 - 1]
-            R2 = 99999999 if cut2 == len(nums_big) else nums_big[cut2]
+        if len(nums1) > len(nums2):
+            return self.findMedianSortedArrays(nums2, nums1)
+        size1 = len(nums1)
+        size2 = len(nums2)
+        count = size1 + size2
+        l = 0
+        r = size1
+        mid = (count + 1) // 2  # 左边个数+左边个数=mid
+        while l <= r:
+            cut1 = (l + r) // 2
+            cut2 = mid - cut1  # 算出第2刀位置
+            L1 = -99999999 if cut1 == 0 else nums1[cut1 - 1]
+            R1 = 99999999 if cut1 == size1 else nums1[cut1]
+            L2 = -99999999 if cut2 == 0 else nums2[cut2 - 1]
+            R2 = 99999999 if cut2 == size2 else nums2[cut2]
             # 二分来求出正确切点
             if L1 > R2:
-                right = cut1 - 1
+                r = cut1 - 1
             elif L2 > R1:
-                left = cut1 + 1
+                l = cut1 + 1
             else:
-                if size % 2 == 0:
+                if count % 2 == 0:
                     return (max(L1, L2) + min(R1, R2)) / 2
                 else:
-                    return min(R1, R2)  # 为什么是R1和R2进行比较,而不是L1,L2比较? youtube上讲解时并没有解释
+                    return max(L1, L2)
 
 
 class Solution1:
@@ -49,8 +51,22 @@ class Solution1:
         return (nums1[median_i - 1] + nums1[median_i]) / 2 if len(nums1) % 2 == 0 else nums1[median_i]
 
 
+class Solution2:
+    def findMedianSortedArrays(self, nums1, nums2):
+        """
+        :param nums1:
+        :param nums2:
+        :return:
+        思入:顶一个findKth的函数,
+        复杂度: O((m+n)log(m+n))
+        """
+
+    def getKth(self, nums1, nums2, k):
+        pass
+
+
 if __name__ == '__main__':
-    nums_1 = [1, 3]
-    nums_2 = [2]
+    nums_1 = [2, 3, 4, 5]
+    nums_2 = [1]
     s = Solution()
     print(s.findMedianSortedArrays(nums_1, nums_2))
